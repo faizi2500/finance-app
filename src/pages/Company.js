4f23e89ede9f429601934349ae58d432a04ff0d2
Faizan Zahid <faizan.zahid2500@gmail.com>
2021-10-15 07:05:39 +0500

    remove uselocation from company details page

--- src/pages/Company.js
+++ src/pages/Company.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
-// import { BrowserRouter as Router } from 'react-router-dom';
-import { useLocation } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCompanyFinancials } from '../redux/company/company';
 import CompanyPage from '../components/CompanyPage';
 import '../components/company.css';
 
 const Company = () => {
-  const data = useLocation();
   const url = window.location.href;
   const newString = url.substring(39);
   const symbol = newString;
@@ -19,9 +16,6 @@ const Company = () => {
   }, [symbol]);
   const companyData = useSelector((state) => state.companyReducer.payload);
   const [newState, setState] = useState(companyData);
-  if (data.name) {
-    setState({});
-  }
   setTimeout(() => {
     setState(companyData);
   }, 2000);
