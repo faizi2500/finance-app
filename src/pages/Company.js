efe5d235a20890628afe46a457e1abec3b29bbc3
Faizan Zahid <faizan.zahid2500@gmail.com>
2021-10-15 07:11:13 +0500

    refactor Company.js to resolve loading bug

--- src/pages/Company.js
+++ src/pages/Company.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCompanyFinancials } from '../redux/company/company';
 import CompanyPage from '../components/CompanyPage';
 import '../components/company.css';
 
 const Company = () => {
+  const data = useLocation();
   const url = window.location.href;
   const newString = url.substring(39);
-  const symbol = newString;
+  let symbol = data.name;
+  if (typeof symbol === 'undefined') {
+    symbol = newString;
+  }
   const dispatch = useDispatch();
   useEffect(() => {
     if (symbol) {
